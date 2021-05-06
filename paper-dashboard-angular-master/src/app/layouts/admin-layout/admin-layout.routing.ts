3c25483757eedbad8606c7004d579ee5353ddbdb
6emre <emre.derman@ug.bilkent.edu.tr>
2021-05-06 23:33:34 +0300

    .

---
+++ paper-dashboard-angular-master/src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -0,0 +1,21 @@
+import { Routes } from '@angular/router';
+
+import { DashboardComponent } from '../../pages/dashboard/dashboard.component';
+import { UserComponent } from '../../pages/user/user.component';
+import { TableComponent } from '../../pages/table/table.component';
+import { TypographyComponent } from '../../pages/typography/typography.component';
+import { RestaurantsComponent } from '../../pages/restaurants/restaurants.component';
+import { MapsComponent } from '../../pages/maps/maps.component';
+import { NotificationsComponent } from '../../pages/notifications/notifications.component';
+import { UpgradeComponent } from '../../pages/upgrade/upgrade.component';
+
+export const AdminLayoutRoutes: Routes = [
+    { path: 'dashboard',      component: DashboardComponent },
+    { path: 'user',           component: UserComponent },
+    { path: 'table',          component: TableComponent },
+    { path: 'typography',     component: TypographyComponent },
+    { path: 'restaurants',          component: RestaurantsComponent },
+    { path: 'maps',           component: MapsComponent },
+    { path: 'notifications',  component: NotificationsComponent },
+    { path: 'upgrade',        component: UpgradeComponent }
+];
