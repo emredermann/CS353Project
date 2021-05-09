0380d484eb8756109ddf9b59251f1e58e29dbee1
6emre <emre.derman@ug.bilkent.edu.tr>
2021-05-10 00:12:39 +0300

    .

--- paper-dashboard-angular-master/src/account/login.component.ts
+++ paper-dashboard-angular-master/src/account/login.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { first } from 'rxjs/operators';
-
-import { AccountService, AlertService } from './_services';
+import { AccountService} from '../_services/account.service';
+import { AlertService } from '../_services/alert.service';
 
 @Component({ templateUrl: 'login.component.html' })
 export class LoginComponent implements OnInit {
