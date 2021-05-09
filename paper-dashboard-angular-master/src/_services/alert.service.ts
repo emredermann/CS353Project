59a4bde13cd44ca626a0068d843bf4944b843a5c
6emre <emre.derman@ug.bilkent.edu.tr>
2021-05-10 00:12:31 +0300

    .

--- paper-dashboard-angular-master/src/_services/alert.service.ts
+++ paper-dashboard-angular-master/src/_services/alert.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable, Subject } from 'rxjs';
 import { filter } from 'rxjs/operators';
 
-import { Alert, AlertType } from '../_models';
+import { Alert, AlertType } from '../_models/alert';
 
 @Injectable({ providedIn: 'root' })
 export class AlertService {
