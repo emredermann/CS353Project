230b207f1c6e6e423caacc82a173bd0e5171cef0
DESKTOP-HROFD7S\bupsd <bupsdogatansel@gmail.com>
2021-05-11 18:29:01 +0300

    .

---
+++ paper-dashboard-angular-master/src/app/layouts/owner-layout/owner-layout.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-owner-layout',
+  templateUrl: './owner-layout.component.html',
+  styleUrls: ['./owner-guy-layout.component.scss']
+})
+export class OwnerLayoutComponent implements OnInit {
+
+  ngOnInit() { }
+}
