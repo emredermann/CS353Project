2a40cb13449fc79a0d2c8c9052eca4983fb9dfe8
DESKTOP-HROFD7S\bupsd <bupsdogatansel@gmail.com>
2021-05-11 23:08:07 +0300

    .

--- paper-dashboard-angular-master/src/app/app.routing.ts
+++ paper-dashboard-angular-master/src/app/app.routing.ts
@@ -8,12 +8,12 @@ export const AppRoutes: Routes = [
     path: '',
     component: AdminLayoutComponent,
     children: [
-<<<<<<< HEAD
+
         {
       path: '',
       loadChildren: './layouts/admin-layout/admin-layout.module#AdminLayoutModule'
   }]},
-=======
+
       {
         path: '',
         loadChildren: './layouts/admin-layout/admin-layout.module#AdminLayoutModule'
@@ -26,56 +26,5 @@ export const AppRoutes: Routes = [
       }
       */
     ]
-  },
-<<<<<<< HEAD
-=======
-  /*
->>>>>>> 984caf584701deb66738bf329dded77e4c4863be
->>>>>>> ebc33e268caf4a869f43c451225bc8478a1c2a23
-  {
-    path: 'user',
-    component: UserLayoutComponent,
-    children: [
-        {
-      path: '',
-      loadChildren: './layouts/user-layout/user-layout.module#UserLayoutModule'
-     }
-    ]
-<<<<<<< HEAD
-  },{
-    path: 'deliver',
-=======
-  },
-  {
-    path: 'delivery',
->>>>>>> 984caf584701deb66738bf329dded77e4c4863be
-    component: DeliveryGuyLayoutComponent,
-    children: [
-        {
-      path: '',
-      loadChildren: './layouts/delivery-guy-layout/delivery-guy.module#DeliveryGuyLayoutModule'
-     }
-    ]
-   }
-  ,{
-    path: 'business',
-    component: OwnerLayoutComponent,
-    children: [
-        {
-      path: '',
-      loadChildren: './layouts/owner-layout/owner-layout.module#OwnerLayoutModule'
-     }
-    ]
-  },
-  /*
-  {
-    path: '',
-    redirectTo: 'login'
-  },
-  */
-  {
-    path: '**',
-    redirectTo: ''
-  }
-]
+
 
