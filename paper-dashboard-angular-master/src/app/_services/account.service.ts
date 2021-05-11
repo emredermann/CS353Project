cde276a254fe3a8638b98e90cf08d992645db252
6emre <emre.derman@ug.bilkent.edu.tr>
2021-05-11 08:41:09 +0300

    .

--- paper-dashboard-angular-master/src/app/_services/account.service.ts
+++ paper-dashboard-angular-master/src/app/_services/account.service.ts
@@ -5,7 +5,7 @@ import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
-import { User } from './../_models/user';
+import { User } from '../_models/user';
 
 @Injectable({ providedIn: 'root' })
 
