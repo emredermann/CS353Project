ae957cf31e092104522374c2c47afd349f36d690
DESKTOP-HROFD7S\bupsd <bupsdogatansel@gmail.com>
2021-05-11 18:24:24 +0300

    .

---
+++ paper-dashboard-angular-master/src/app/layouts/delivery-guy-layout/delivery-guy-layout.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-delivery-guy-layout',
+  templateUrl: './delivery-guy-layout.component.html',
+  styleUrls: ['./delivery-guy-layout.component.scss']
+})
+export class DeliveryGuyLayoutComponent implements OnInit {
+
+  ngOnInit() { }
+}
---
+++ paper-dashboard-angular-master/src/app/layouts/delivery-guy-layout/delivery-guy-layout.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { DeliveryGuyLayoutRoutes } from './delivery-guy-layout.routing';
+import { DashboardComponent }       from '../../pages/dashboard/dashboard.component';
+import { DeliverComponent }            from '../../pages/deliver/deliver.component';
+import { TableComponent }           from '../../pages/table/table.component';
+import { MapsComponent }            from '../../pages/maps/maps.component';
+//import { BusinessComponent }   from '../../pages/business/business.component';
+import { LoginComponent }         from '../../pages/login/login.component';
+//import { UserComponent } from '../../pages/user/user.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forChild(DeliveryGuyLayoutRoutes),
+    FormsModule,
+    NgbModule
+  ],
+  declarations: [
+    DashboardComponent,
+    DeliverComponent,
+    TableComponent,
+    LoginComponent,
+    MapsComponent,
+    //BusinessComponent,
+    //UserComponent
+  ]
+})
+
+export class DeliveryGuyLayoutModule {}
