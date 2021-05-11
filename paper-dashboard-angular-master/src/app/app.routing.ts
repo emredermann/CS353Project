b03a1d5cebbdcabfcc1604d519262ecca58c18b7
6emre <emre.derman@ug.bilkent.edu.tr>
2021-05-11 21:58:15 +0300

    .

--- paper-dashboard-angular-master/src/app/app.routing.ts
+++ paper-dashboard-angular-master/src/app/app.routing.ts
@@ -1,49 +1,44 @@
 import { Routes } from '@angular/router';
-
-import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
-import { UserLayoutComponent } from './layouts/user-layout/user-layout.component';
 import { DeliveryGuyLayoutComponent } from './layouts/delivery-guy-layout/delivery-guy-layout.component';
 import { OwnerLayoutComponent } from './layouts/owner-layout/owner-layout.component';
-
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { UserLayoutComponent } from './layouts/user-layout/user-layout.component';
 export const AppRoutes: Routes = [
   {
     path: 'login',
     component: AdminLayoutComponent,
     children: [
-      {
-        path: '',
-        loadChildren: './layouts/admin-layout/admin-layout.module#AdminLayoutModule'
-      }
-    ]
-  },
+        {
+      path: '',
+      loadChildren: './layouts/admin-layout/admin-layout.module#AdminLayoutModule'
+  }]},
   {
     path: 'user',
     component: UserLayoutComponent,
     children: [
-      {
-        path: '',
-        loadChildren: './layouts/user-layout/user-layout.module#UserLayoutModule'
-      }
+        {
+      path: '',
+      loadChildren: './layouts/user-layout/user-layout.module#UserLayoutModule'
+     }
     ]
-  },
-  {
+  },{
     path: 'deliver',
     component: DeliveryGuyLayoutComponent,
     children: [
-      {
-        path: '',
-        loadChildren: './layouts/delivery-guy-layout/delivery-guy-layout.module#DeliveryGuyLayoutModule'
-      }
+        {
+      path: '',
+      loadChildren: './layouts/delivery-guy-layout/delivery-guy.module#DeliveryGuyLayoutModule'
+     }
     ]
-  },
-  {
+   }
+  ,{
     path: 'business',
     component: OwnerLayoutComponent,
     children: [
-      {
-        path: '',
-        loadChildren: './layouts/owner-layout/owner-layout.module#OwnerLayoutModule'
-      }
+        {
+      path: '',
+      loadChildren: './layouts/owner-layout/owner-layout.module#OwnerLayoutModule'
+     }
     ]
   },
   {
