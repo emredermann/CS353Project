cb65cbb3c76677e8e598494cb2fcc2b937e0e66d
DESKTOP-HROFD7S\bupsd <bupsdogatansel@gmail.com>
2021-05-11 15:36:41 +0300

    .

---
+++ paper-dashboard-angular-master/src/app/layouts/user-layout/user-layout.routing.ts
@@ -0,0 +1,18 @@
+import { Routes } from '@angular/router';
+import { DashboardComponent } from '../../pages/dashboard/dashboard.component';
+//import { DeliverComponent } from '../../pages/deliver/deliver.component';
+import { TableComponent } from '../../pages/table/table.component';
+import { MapsComponent } from '../../pages/maps/maps.component';
+//import { BusinessComponent } from '../../pages/business/business.component';
+import { LoginComponent } from '../../pages/login/login.component';
+//import { UserComponent } from '../../pages/user/user.component';
+
+export const UserLayoutRoutes: Routes = [
+    { path: 'dashboard', component: DashboardComponent },
+    //{ path: 'deliver',    component: DeliverComponent },
+    { path: 'table',     component: TableComponent },
+    { path: 'maps',      component: MapsComponent },
+    //{ path: 'business',  component: BusinessComponent },
+    //{ path: 'user',     component: UserComponent },
+    { path: 'login',     component: LoginComponent }
+];
