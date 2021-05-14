ab4f84585178b2a0d937585f6ad7d13fbe7bfdd7
6emre <emre.derman@ug.bilkent.edu.tr>
2021-05-15 00:14:03 +0300

    .

--- paper-dashboard-angular-master/src/app/_models/userOrder.ts
+++ paper-dashboard-angular-master/src/app/_models/userOrder.ts
@@ -3,10 +3,12 @@ export class userOrder{
     items: any[];
     date:   Date;
     price: number;
+    restaurantName:string;
     restaurantReview:string;
     restaurantRating: number;
     delGuyReview: string;
     delGuyRating:number;
     restaurantResponse:string;
-    orderState:boolean;
+    orderState:string;
+
 }
