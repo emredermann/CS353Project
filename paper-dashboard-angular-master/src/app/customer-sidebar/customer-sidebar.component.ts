198b07aa89e53598a821c5b4fd480c9a6be48795
DESKTOP-HROFD7S\bupsd <bupsdogatansel@gmail.com>
2021-05-14 14:19:19 +0300

    .

---
+++ paper-dashboard-angular-master/src/app/customer-sidebar/customer-sidebar.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+
+
+export interface RouteInfo {
+    path: string;
+    title: string;
+    icon: string;
+    class: string;
+}
+
+
+export const ROUTES: RouteInfo[] = [
+    { path: '/login',           title: 'Log In',    icon:'nc-spaceship',  class: '' },
+    { path: '/user',            title: 'Sign Up To User',    icon:'nc-bell-55',  class: '' },
+    { path: '/table',           title: 'Available Restaurants',        icon:'nc-tile-56',    class: '' },
+    { path: '/order-history',   title: 'Order History',      icon:'nc-single-02',  class: '' },
+    { path: '/combo',           title: 'Customize Combos',     icon:'nc-bank',    class: '' },
+    { path: '/combo-order',   title: 'Order Combos',      icon:'nc-single-02',  class: '' }
+];
+
+
+@Component({
+    moduleId: module.id,
+    selector: 'customer-sidebar-cmp',
+    templateUrl: 'customer-sidebar.component.html',
+})
+
+export class CustomerSidebarComponent implements OnInit {
+    public menuItems: any[];
+    ngOnInit() {
+        this.menuItems = ROUTES.filter(menuItem => menuItem);
+    }
+}
---
+++ paper-dashboard-angular-master/src/app/owner-sidebar/owner-sidebar.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+
+export interface RouteInfo {
+    path: string;
+    title: string;
+    icon: string;
+    class: string;
+}
+
+export const ROUTES: RouteInfo[] = [
+    { path: '/login',               title: 'Log In',    icon:'nc-spaceship',  class: '' },
+    { path: '/business', title: 'Add Business Account',     icon:'nc-bank',    class: '' },
+    { path: '/comments',    title: 'View Reviews and Ratings',     icon:'nc-bank',    class: '' },
+    { path: '/menu-editor',       title: 'Edit Menu',      icon:'nc-single-02',  class: '' },
+];
+
+
+@Component({
+    moduleId: module.id,
+    selector: 'owner-sidebar-cmp',
+    templateUrl: 'owner-sidebar.component.html',
+})
+
+export class OwnerSidebarComponent implements OnInit {
+    public menuItems: any[];
+    ngOnInit() {
+        this.menuItems = ROUTES.filter(menuItem => menuItem);
+    }
+}
