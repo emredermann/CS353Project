dd8dee9402e60e3446f339d502d6c30d162a5802
6emre <emre.derman@ug.bilkent.edu.tr>
2021-05-15 16:54:20 +0300

    .

--- paper-dashboard-angular-master/src/app/pages/delivery-guy-new-assignments/delivery-guy-new-assignments.component.ts
+++ paper-dashboard-angular-master/src/app/pages/delivery-guy-new-assignments/delivery-guy-new-assignments.component.ts
@@ -1,6 +1,8 @@
 import { userOrder } from './../../_models/userOrder';
 import { deliveryGuy } from './../../_models/deliveryGuy';
 import { Component, OnInit } from '@angular/core';
+import { AssignmentExpression } from 'typescript';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 
 @Component({
@@ -10,6 +12,14 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class DeliveryGuyNewAssignmentsComponent implements OnInit{
+    
+    loginForm = new FormGroup({
+        type : new FormControl('',Validators.required),
+    });
+
+    constructor( private formBuilder: FormBuilder){}
+
+
     public delGuyInfo: deliveryGuy = {deliveryGuyName: "İhsan Vekil", job: "Delivery Guy", rating: 3.5, joinedOn: "4.3.2020", status :"available"};
     public searchText: string;
     public title = 'New Delivery Assignments';
@@ -18,9 +28,12 @@ export class DeliveryGuyNewAssignmentsComponent implements OnInit{
     public selectedRegion: string ;
     public filteredRestaurantName:string;
     public filteredAssignmentCount:number;
+    public counter:number;
     public currentAssignment: number;
+    public searchBox= false;
+
     //There will be one assignment
-    assignments: userOrder []=[{ customerName: "Dr.Who", idNo:1,
+    public assignments: userOrder []=[{ customerName: "Dr.Who", idNo:1,
         items:['Quarterpounder Cheeseburger with fries', 'Coke Zero (35 mL)', 'Total: $35'],
         date: new Date(), 
         price: 100,
@@ -52,9 +65,11 @@ export class DeliveryGuyNewAssignmentsComponent implements OnInit{
         restaurantResponse:"Good customer",
         orderState: "pending"},];
 
-        filteredAssignment:userOrder [];
-
+       public filteredAssignment:userOrder [];
+       public resultAssignment:userOrder [];
+       
     ngOnInit(){ //Database'den çekilecek kısım bu
+        this.resultAssignment = this.assignments;
     }
     regionSet(e){
         this.selectedRegion = e.target.value;
@@ -75,10 +90,40 @@ export class DeliveryGuyNewAssignmentsComponent implements OnInit{
     getStatus(){
         return this.delGuyInfo.status;
     }
+    refreshFilter(){
+        this.resultAssignment = this.assignments;
+    }
 
+    submitRestaurantFilter(){
+        this.counter = 0;
+        /*
+        for (var val of this.assignments) {
+            if(this.filteredRestaurantName === val.restaurantName){
+                alert(val.restaurantName);
+                
+                for (let index = 1; index < 12; index++) {
+                   this.filteredAssignment[index].customerName=val.customerName
+                   this.filteredAssignment[index].date = val.date;
+                   this.filteredAssignment[index].delGuyRating = val.delGuyRating;
+                   this.filteredAssignment[index].delGuyReview = val.delGuyReview;
+                   this.filteredAssignment[index].idNo = val.idNo;
+                   this.filteredAssignment[index].items = val.items;
+                   this.filteredAssignment[index].orderState = val.orderState;
+                   this.filteredAssignment[index].price = val.price;
+                   this.filteredAssignment[index].restaurantName = val.restaurantName;
+                   this.filteredAssignment[index].restaurantRating = val.restaurantRating;
+                   this.filteredAssignment[index].restaurantResponse = val.restaurantResponse;
+                   this.filteredAssignment[index].restaurantReview = val.restaurantReview;
+                }
+                 this.counter = this.counter+ 1;
+            }
+          }*/
+          
+          this.resultAssignment=this.assignments.filter(item=> item.restaurantName ===this.filteredRestaurantName);
+          
+    }
     changeRestaurantSelection(e){
-            this.filteredRestaurantName = e.value.target;
-            alert( this.filteredRestaurantName);
+            this.filteredRestaurantName = e.target.value;
     }
     declineAssignment(){
            this.delGuyInfo.status = "available"; 
